730b7f4cf1bfc8a3a777dfd0c92066f5127e98d0
Steve Higgs <steve.higgs@salecycle.com>
2016-08-04 08:00:16 +0100

    make `npm start` start the site

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
 const es6Pipeline = require('gulp-webpack-es6-pipeline');
 
 es6Pipeline.registerBuildGulpTasks(
@@ -10,3 +11,20 @@ es6Pipeline.registerBuildGulpTasks(
     outputDir: __dirname + '/.build'
   }
 );
+
+gulp.task('runSite', (done) => {
+  nodemon({
+    script: 'src/server.js',
+    ext: 'js html',
+    env: {
+      NODE_ENV: 'development'
+    }
+  });
+
+  setTimeout(() => {
+    done();
+  }, 500);
+});
+
+gulp.task('default', ['es6Pipeline:build:dev', 'runSite']);
+gulp.task('dev', ['es6Pipeline:watch', 'runSite']);
