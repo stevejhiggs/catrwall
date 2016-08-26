ed7e7b77fe2a0f37cb0078111fbe95d995fa7a0f
steve higgs <stevejhiggs@hotmail.com>
2016-08-26 18:49:57 +0100

    recreate the cats collection if it doesn't exist

--- src/tasks/getCats.js
+++ src/tasks/getCats.js
@@ -5,6 +5,7 @@ const uuid = require('uuid');
 const srcRegex = /<img src="(.*)">/gi;
 
 const getCatsFromApi = (count) => {
+
     return new Promise((resolve, reject) => {
         fetch(`http://thecatapi.com/api/images/get?size=large&type=gif&results_per_page=${count}&format=html`)
         .then(res => {
@@ -21,18 +22,25 @@ const getCatsFromApi = (count) => {
 
 const recreateCatsInDb = (catImages) => {
     let catTable;
-    // delete then recreate the cats
-    return db.table('cats').delete().run()
+    return db.tableList().run()
+    .then((tables) => {
+      if (!tables.includes('cats')) {
+        return db.tableCreate('cats');
+      }
+    })
     .then(() => {
-        const catDocs = catImages.map(catImage => {
-            return {
-                id: uuid.v4(),
-                src: catImage,
-                votes: 0
-            }
-        });
+      return db.table('cats').delete().run();
+    })
+    .then(() => {
+      const catDocs = catImages.map(catImage => {
+        return {
+          id: uuid.v4(),
+          src: catImage,
+          votes: 0
+        }
+      });
 
-        return db.table('cats').insert(catDocs).run();
+      return db.table('cats').insert(catDocs).run();
     });
 }
 
